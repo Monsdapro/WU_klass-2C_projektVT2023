d28bb22823708bb343ed378852152dc11e071145
Monsdapro <95749590+Monsdapro@users.noreply.github.com>
2023-01-31 14:39:59 +0100

    Påbörjat meny för mobil

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggle_meny() {
+    document.getElementById('burger_icon').classList.toggle('burger_meny_showing')
+
+    document.getElementById('burger_meny_list').classList.toggle('burger_meny_showing')
+}
+
+document.getElementById('burger_icon').addEventListener('click', toggle_meny)
