95e0a86b6bce84f48cb71e76fcb53490e9a8e2c7
Monsdapro <95749590+Monsdapro@users.noreply.github.com>
2023-02-03 14:04:11 +0100

    Lite arbete på programeringen

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ function toggle_meny() {
     document.getElementById('burger_icon').classList.toggle('burger_meny_showing')
 
     document.getElementById('burger_meny_list').classList.toggle('burger_meny_showing')
+
+    document.getElementById('grid_parent').classList.toggle('burger_meny_showing')
 }
 
 document.getElementById('burger_icon').addEventListener('click', toggle_meny)
