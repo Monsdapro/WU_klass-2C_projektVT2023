cd7d3810da229d0feb9d5439e0504f911c00e0e8
Monsdapro <95749590+Monsdapro@users.noreply.github.com>
2023-03-14 12:54:20 +0100

    Vecka 10 arbete?

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
+var audio = new Audio('/misc/roblox-cheeseburger-sound-effect-By-Tuna.mp3');
+
 function toggle_meny() {
     document.getElementById('burger_icon').classList.toggle('burger_meny_showing')
 
     document.getElementById('burger_meny_list').classList.toggle('burger_meny_showing')
 
     document.getElementById('grid_parent').classList.toggle('burger_meny_showing')
+    
+    audio.play();
 }
 
 document.getElementById('burger_icon').addEventListener('click', toggle_meny)
